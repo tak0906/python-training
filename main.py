b4f43ab234173910843f396a7f6510cce3938d58
tak0906 <swimmingcludtak@gmail.com>
2026-03-08 14:37:19 +0900

    Day4:Add detail hint to number guessing game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 name=input("名前を入力してください：")
 age=input("年齢を入力してください：")
 print("こんにちは"+name+"!")
@@ -49,6 +51,44 @@ for i in range(1,20):
 if not correct:print("ざんね~~~~んww正解は",seikai)
 
 import random
+seikai=random.randint(1,319)
+correct=False
+
+for i in range(14):
+    kaitou=int(input("当たりだと思う数字（1~319）を入力して正解してな～～："))
+    sa=abs(kaitou-seikai)
+    nokori=(13-i)
+
+    if kaitou==seikai:
+        print("大当たり~~~~~~~~!!")
+        correct=True
+        break
+    elif 51<=sa<=100 and kaitou<=seikai:
+        hint="50~100でかくせなかんな~まだ遠いで~~"
+    elif 51<=sa<=100 and seikai<=kaitou:
+        hint="50~100ちっさくせなかんな~まだ遠いで～～"
+    elif 11<=sa<=50 and kaitou<=seikai:
+        hint="ちょっと惜しい、、10~50でかくしてや～"
+    elif 11<=sa<=50 and seikai<=kaitou:
+        hint="ちょっと惜しい、、10~50ちっさくしてや～"
+    elif 1<=sa<=10 and kaitou<=seikai:
+        hint="だいぶ近い、、!1~10でかくするんやで！"
+    elif 1<=sa<=10 and seikai<=kaitou:
+        hint="だいぶ近い、、!1~10ちっさくするんや！"
+    elif 101<=sa<=200 and kaitou<=seikai:
+        hint="100~200でかくせなかんで~~まだ遠すぎて草"
+    elif 101<=sa<=200 and seikai<=kaitou:
+        hint="100~200ちっさくせなかんで~~まだ遠すぎて草"
+    elif 200<sa and kaitou<=seikai:
+        hint="200以上でかくして~遠すぎてワロタ"
+    elif 200<sa and seikai<=kaitou:
+        hint="200以上ちっさくして~遠すぎてワロタ"
+    print(f"外れWWあと{nokori}回であててな～ちなみに{hint}")
+    
+    if nokori==0:print(f"ざんね~~~~んww正解は{seikai}")
+
+
+import random
 atari=random.randint(1,319)
 correct=False
 
@@ -59,3 +99,35 @@ while True:
          correct=True
          break
     else:print("外れw")
+
+import random
+seikai=random.randint(1,319)
+
+for i in range(15):
+    kaitou=int(input("当たりだと思う数字（1~319）を入力して正解してな～～："))
+    sa=abs(kaitou-seikai)
+    nokori=(14-i)
+
+    if kaitou<seikai:
+        direction="でかく"
+    else:
+        direction="ちっさく"
+
+    if kaitou==seikai:
+        print("大当たり~~~~~~~~!!")
+        break
+    elif 51<=sa<=100:
+        hint=(f"50~100{direction}せなかんな~まだ遠いで~~")
+    elif 11<=sa<=50:
+        hint=(f"ちょっと惜しい、、10~50{direction}してや～")
+    elif 1<=sa<=10:
+        hint=(f"だいぶ近い、、!1~10{direction}するんやで！")
+    elif 101<=sa<=200:
+        hint=(f"100~200{direction}せなかんで~~まだ遠すぎて草")
+    elif 200<sa:
+        hint=(f"200以上{direction}して~遠すぎてワロタ")
+    
+    if nokori>0:
+        print(f"外れWWあと{nokori}回であててな～ちなみに{hint}")
+    else:
+        print(f"ざんね~~~~んww正解は{seikai}!")
