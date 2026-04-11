5c0f225dcb77260c8eb6a588750f91f6eae1cff1
tak0906 <swimmingcludtak@gmail.com>
2026-04-11 20:46:44 +0900

    Day15:HTML

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import random
 
 app = Flask(__name__)
 
+app.secret_key = "secret_key"
+
 seikai = random.randint(1, 100)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if "seikai" not in session:
+        session["seikai"] = random.randint(1, 100)
+
     message = ""
 
     if request.method == "POST":
-        guess = int(request.form["guess"])
+        try:
+            guess = int(request.form["guess"])
 
-        if guess == seikai:
-            message = "正解！！"
-        elif guess < seikai:
-            message = "もっと大きいで！"
-        else:
-            message = "もっと小さいで！"
+            if guess == session["seikai"]:
+                message = "正解！！"
+                session.pop("seikai")  # リセット
+            elif guess < session["seikai"]:
+                message = "もっと大きいで！"
+            else:
+                message = "もっと小さいで！"
+        except ValueError:
+            message = "数字を入力してな～！"
 
     return render_template("index.html", message=message)
 
