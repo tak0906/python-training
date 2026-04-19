80f1be0dcab1a7a79c525f941cbb3524ebad84d6
tak0906 <swimmingcludtak@gmail.com>
2026-04-19 16:20:24 +0900

    Day16:Add HTML

--- app.py
+++ app.py
@@ -5,31 +5,44 @@ app = Flask(__name__)
 
 app.secret_key = "secret_key"
 
-seikai = random.randint(1, 100)
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if "seikai" not in session:
         session["seikai"] = random.randint(1, 100)
+    
+    if "count" not in session:
+        session["count"] = 10
 
     message = ""
+    hint = ""
+    error = ""
 
     if request.method == "POST":
+        if "reset" in request.form:
+            session.clear()
+
         try:
             guess = int(request.form["guess"])
+            session["count"] -= 1
 
             if guess == session["seikai"]:
-                message = "正解！！"
-                session.pop("seikai")  # リセット
-            elif guess < session["seikai"]:
-                message = "もっと大きいで！"
-            else:
-                message = "もっと小さいで！"
-        except ValueError:
-            message = "数字を入力してな～！"
+                 message = "正解！！"
+                 session.clear()
 
-    return render_template("index.html", message=message)
+            elif session["count"] <= 0:
+                message = f"ゲームオーバー！正解は{session['seikai']}"
+                
+            elif guess < session["seikai"]:
+                hint = f"もっと大きいで！残り{session['count']}回！"
+
+            else:
+                hint = f"もっと小さいで！残り{session['count']}回！"
+
+        except ValueError:
+            error = "数字を入力してな～！"
+
+    return render_template("index.html", message = message,hint = hint,error=error,count=session.get("count"))
 
 if __name__ == "__main__":
     app.run(debug=True)
